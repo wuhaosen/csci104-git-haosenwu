eb27b539fcbef6f8008c5c2a6c0b17e33c956037
wuhaosen <haosenwu@usc.edu>
2017-08-24 17:31:22 -0700

    My first HelloWorld using git

---
+++ prac_0824.cpp
@@ -0,0 +1,16 @@
+//int first;
+string second,fourth;
+char third;
+stringstream ss;
+ss << "Minecraft" ;
+ss >> first;
+ss >> second;
+--------------------//
+
+# include <iostream>
+
+int main() 
+{
+  std::cout << "HelloWorld!" << std::endl;
+}
+
